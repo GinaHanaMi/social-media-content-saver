ca70e6a58a7d0de8d240daa9222fa4955d6984bc
GinaHanaMi <lukasz.wojcik.business@gmail.com>
2025-04-17 13:48:24 +0200

    many changes

--- app/src/main/java/com/example/socialmediacontentsaver/receiveData/FolderRecyclerViewAdapter.java
+++ app/src/main/java/com/example/socialmediacontentsaver/receiveData/FolderRecyclerViewAdapter.java
@@ -1,14 +1,27 @@
 package com.example.socialmediacontentsaver.receiveData;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.socialmediacontentsaver.R;
+import com.example.socialmediacontentsaver.models.FolderModel;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class FolderRecyclerViewAdapter extends RecyclerView.Adapter<FolderRecyclerViewAdapter.MyViewHolder> {
+    Context context;
+    ArrayList<FolderModel> folderModels;
+
+    public FolderRecyclerViewAdapter(Context context, ArrayList<FolderModel> folderModels) {
+        this.context = context;
+        this.folderModels = folderModels;
+    }
 
     @NonNull
     @Override
@@ -21,12 +34,18 @@ public class FolderRecyclerViewAdapter extends RecyclerView.Adapter<FolderRecycl
     public void onBindViewHolder(@NonNull FolderRecyclerViewAdapter.MyViewHolder holder, int position) {
         // assigning values to each rows as they come back to screen
         // based on position of the recycler view
+
+        holder.thumbnailRecycleViewTextViewVar.setImageResource();
+        holder.titleRecyclerTextViewVar.setText(folderModels.get(position).getTitle());
+        holder.descriptionRecyclerTextViewVar.setText(folderModels.get(position).getDescription());
+        holder.createAtRecyclerTextViewVar.setText(folderModels.get(position).getCreated_at());
+
     }
 
     @Override
     public int getItemCount() {
         // the recycler view just wants to know the number of items that are beeing displayed
-        return 0;
+        return folderModels.size();
     }
 
 
@@ -34,15 +53,17 @@ public class FolderRecyclerViewAdapter extends RecyclerView.Adapter<FolderRecycl
         // Grabing the views from recycler_view_row layout file
         // kinda like oncreate
 
-
-//        ImageView mediaThumbnail;
-//        TextView
-
+        ImageView thumbnailRecycleViewTextViewVar;
+        TextView titleRecyclerTextViewVar, descriptionRecyclerTextViewVar, createAtRecyclerTextViewVar;
 
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
 
+            thumbnailRecycleViewTextViewVar = itemView.findViewById(R.id.thumbnailRecycleViewTextView);
+            titleRecyclerTextViewVar = itemView.findViewById(R.id.titleRecyclerTextView);
+            descriptionRecyclerTextViewVar = itemView.findViewById(R.id.descriptionRecyclerTextView);
+            createAtRecyclerTextViewVar = itemView.findViewById(R.id.createAtRecyclerTextView);
 
         }
     }
