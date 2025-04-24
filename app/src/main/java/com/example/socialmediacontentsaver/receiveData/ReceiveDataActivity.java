b00b013f60a64212258a0f7c45776b7c5d3fc686
GinaHanaMi <lukasz.wojcik.business@gmail.com>
2025-04-24 18:55:30 +0200

    Different approach

--- app/src/main/java/com/example/socialmediacontentsaver/receiveData/ReceiveDataActivity.java
+++ app/src/main/java/com/example/socialmediacontentsaver/receiveData/ReceiveDataActivity.java
@@ -12,7 +12,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResultLauncher;
