601391a8048a421857b1034b1f10a0890906f3f3
GinaHanaMi <lukasz.wojcik.business@gmail.com>
2025-04-25 18:07:23 +0200

    works, tab 1 and 2 created

--- app/src/main/java/com/example/socialmediacontentsaver/mainView/foldersActivityClasses/FoldersActivityRecyclerViewAdapter.java
+++ app/src/main/java/com/example/socialmediacontentsaver/mainView/foldersActivityClasses/FoldersActivityRecyclerViewAdapter.java
@@ -1,4 +1,86 @@
 package com.example.socialmediacontentsaver.mainView.foldersActivityClasses;
 
-public class FoldersActivityRecyclerViewAdapter {
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.socialmediacontentsaver.R;
+import com.example.socialmediacontentsaver.models.FolderModel;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class FoldersActivityRecyclerViewAdapter extends RecyclerView.Adapter<FoldersActivityRecyclerViewAdapter.MyViewHolder>{
+    Context context;
+    ArrayList<FolderModel> mainFolderModels;
+
+    public FoldersActivityRecyclerViewAdapter(Context context, ArrayList<FolderModel> mainFolderModels) {
+        this.context = context;
+        this.mainFolderModels = mainFolderModels;
+    }
+
+    @NonNull
+    @Override
+    public FoldersActivityRecyclerViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Where the layout gets inflated and it gives look to rows
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.folder_recycler_view_row, parent, false);
+
+        return new FoldersActivityRecyclerViewAdapter.MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FoldersActivityRecyclerViewAdapter.MyViewHolder holder, int position) {
+        FolderModel mainFolderModel = mainFolderModels.get(position);
+
+        String thumbnailPath = mainFolderModel.getThumbnail();
+
+        Uri thumbnailUri;
+        if (thumbnailPath.startsWith("content://") || thumbnailPath.startsWith("file://")) {
+            thumbnailUri = Uri.parse(thumbnailPath);
+        } else {
+            thumbnailUri = Uri.fromFile(new File(thumbnailPath));
+        }
+
+        Glide.with(context)
+                .load(thumbnailUri)
+                .placeholder(R.drawable.ic_launcher_background)
+                .error(R.drawable.ic_launcher_foreground)
+                .into(holder.folderThumbnailRecycleViewImageViewVar);
+
+        holder.folderTitleRecyclerTextViewVar.setText(mainFolderModel.getTitle());
+        holder.folderDescriptionRecyclerTextViewVar.setText(mainFolderModel.getDescription());
+        holder.folderCreatedAtRecyclerTextViewVar.setText(mainFolderModel.getCreated_at());
+    }
+
+    @Override
+    public int getItemCount() {
+        // the recycler view just wants to know the number of items that are beeing displayed
+        return mainFolderModels.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        // Grabing the views from recycler_view_row layout file
+        // kinda like oncreate
+        ImageView folderThumbnailRecycleViewImageViewVar;
+        TextView folderTitleRecyclerTextViewVar, folderDescriptionRecyclerTextViewVar, folderCreatedAtRecyclerTextViewVar;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            folderThumbnailRecycleViewImageViewVar = itemView.findViewById(R.id.folderThumbnailRecycleViewImageView);
+            folderTitleRecyclerTextViewVar = itemView.findViewById(R.id.folderTitleRecyclerTextView);
+            folderDescriptionRecyclerTextViewVar = itemView.findViewById(R.id.folderDescriptionRecyclerTextView);
+            folderCreatedAtRecyclerTextViewVar = itemView.findViewById(R.id.folderCreatedAtRecyclerTextView);
+        }
+    }
 }
--- app/src/main/java/com/example/socialmediacontentsaver/mainView/foldersActivityClasses/FragmentTwo.java
+++ app/src/main/java/com/example/socialmediacontentsaver/mainView/foldersActivityClasses/FragmentTwo.java
@@ -1,22 +1,61 @@
 package com.example.socialmediacontentsaver.mainView.foldersActivityClasses;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.socialmediacontentsaver.R;
+import com.example.socialmediacontentsaver.databaseHelpers.AppDatabaseHelper;
+import com.example.socialmediacontentsaver.databaseHelpers.FolderDatabaseHelper;
+import com.example.socialmediacontentsaver.models.FolderModel;
+
+import java.util.ArrayList;
 
 
 public class FragmentTwo extends Fragment {
+    FolderDatabaseHelper mainFolderDatabase;
+    FoldersActivityRecyclerViewAdapter mainAdapter;
+    ArrayList<FolderModel> mainFoldersModels = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_two, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_two, container, false);
+
+        AppDatabaseHelper appDatabaseHelper = new AppDatabaseHelper(requireContext());
+        SQLiteDatabase db = appDatabaseHelper.getWritableDatabase();
+        mainFolderDatabase = new FolderDatabaseHelper(db);
+
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        FoldersPopulateLayoutWithFolders();
+    }
+
+    public void FoldersPopulateLayoutWithFolders() {
+        mainFoldersModels.clear();
+        Cursor res = mainFolderDatabase.getAllFolders();
+
+        RecyclerView mainRecyclerView = getView().findViewById(R.id.folderRecyclerView);
+
+        while (res.moveToNext()) {
+            mainFoldersModels.add(new FolderModel(res.getString(0), res.getString(1), res.getString(2), res.getString(3), res.getString(4)));
+        }
+
+        mainAdapter = new FoldersActivityRecyclerViewAdapter(requireContext(), mainFoldersModels);
+
+        mainRecyclerView.setAdapter(mainAdapter);
+        mainRecyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
     }
 }
