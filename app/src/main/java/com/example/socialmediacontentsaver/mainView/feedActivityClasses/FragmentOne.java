78ce357fd735a25448fa371dd41606f8ffdb5c08
GinaHanaMi <lukasz.wojcik.business@gmail.com>
2025-04-29 17:13:20 +0200

    Update FragmentOne.java

--- app/src/main/java/com/example/socialmediacontentsaver/mainView/feedActivityClasses/FragmentOne.java
+++ app/src/main/java/com/example/socialmediacontentsaver/mainView/feedActivityClasses/FragmentOne.java
@@ -1,9 +1,13 @@
 package com.example.socialmediacontentsaver.mainView.feedActivityClasses;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,6 +15,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.SearchView;
 import android.widget.Toast;
 
@@ -18,8 +25,13 @@ import com.example.socialmediacontentsaver.R;
 import com.example.socialmediacontentsaver.databaseHelpers.AppDatabaseHelper;
 import com.example.socialmediacontentsaver.databaseHelpers.ContentDatabaseHelper;
 import com.example.socialmediacontentsaver.models.ContentModel;
+import com.example.socialmediacontentsaver.receiveData.ReceiveDataActivity;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class FragmentOne extends Fragment implements FeedRecyclerViewInterface {
     ContentDatabaseHelper contentDatabase;
@@ -35,6 +47,7 @@ public class FragmentOne extends Fragment implements FeedRecyclerViewInterface {
         AppDatabaseHelper appDatabaseHelper = new AppDatabaseHelper(requireContext());
         SQLiteDatabase db = appDatabaseHelper.getWritableDatabase();
         contentDatabase = new ContentDatabaseHelper(db);
+        setupActionOnFeedContentDialog();
 
         return rootView;
     }
@@ -92,7 +105,50 @@ public class FragmentOne extends Fragment implements FeedRecyclerViewInterface {
 
     @Override
     public void onFeedItemClick(int position) {
-        Toast.makeText(getActivity(), contentModels.get(position).getTitle(),
-                Toast.LENGTH_LONG).show();
+        View dialogView = getLayoutInflater().inflate(R.layout.feed_content_dialog, null);
+
+        Button feedDialogOpenContentButton = dialogView.findViewById(R.id.feedDialogOpenContentButton);
+        Button feedDialogEditContentButton = dialogView.findViewById(R.id.feedDialogEditContentButton);
+        Button feedDialogShareContentButton = dialogView.findViewById(R.id.feedDialogShareContentButton);
+        Button feedDialogDeleteContentButton = dialogView.findViewById(R.id.feedDialogDeleteContentButton);
+
+        AlertDialog dialog = new AlertDialog.Builder(requireContext())
+                .setView(dialogView)
+                .create();
+
+        dialog.show();
+
+        feedDialogOpenContentButton.setOnClickListener(view -> {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(contentModels.get(position).getLink()));
+            startActivity(browserIntent);
+            dialog.dismiss();
+        });
+
+        feedDialogEditContentButton.setOnClickListener(view -> {
+            Toast.makeText(getActivity(), "feedEdit",
+                    Toast.LENGTH_LONG).show();
+            dialog.dismiss();
+        });
+
+        feedDialogShareContentButton.setOnClickListener(view -> {
+            Intent sendContentURLIntent = new Intent();
+            sendContentURLIntent.setAction(Intent.ACTION_SEND);
+            sendContentURLIntent.putExtra(Intent.EXTRA_TEXT, contentModels.get(position).getLink());
+            sendContentURLIntent.setType("text/plain");
+
+            Intent shareIntent = Intent.createChooser(sendContentURLIntent, null);
+            startActivity(shareIntent);
+
+            dialog.dismiss();
+        });
+
+        feedDialogDeleteContentButton.setOnClickListener(view -> {
+            Toast.makeText(getActivity(), "feedDelete",
+                    Toast.LENGTH_LONG).show();
+            dialog.dismiss();
+        });
+
+    }
+    private void setupActionOnFeedContentDialog() {
     }
 }
